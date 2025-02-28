7e866543954a778d5c2d2bbb0501c9113c1fbd66
cntencra <cn10cra@gmail.com>
2025-02-28 16:37:09 +0000

    Completed utils functions

--- db/seeds/utils.js
+++ db/seeds/utils.js
@@ -5,5 +5,26 @@ exports.convertTimestampToDate = ({ created_at, ...otherProperties }) => {
   return { created_at: new Date(created_at), ...otherProperties };
 };
 
+exports.formatComments = (commentData, articleRows) => {
+  
+  const lookUp = this.articleLookup(articleRows);
+ 
+  return commentData.map(({article_title, ...otherProperties}) => {
+
+    if(!article_title) return {...otherProperties}
+
+    const newComment = {...otherProperties};
+    newComment.article_id = lookUp[article_title];
+    return  newComment;
+  })
+}
+
+exports.articleLookup = (articleData) => {
+  return articleData.reduce((accumulator, article)=> {
+    accumulator[article.title] = article.article_id;
+    return accumulator;
+  }, {})
+}
+
 
 
