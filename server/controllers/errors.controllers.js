cb3c8206b6ddcf8adb6d8d76b42cb46ae6fe249e
cntencra <cn10cra@gmail.com>
2025-03-10 15:47:31 +0000

    custom errors

--- server/controllers/errors.controllers.js
+++ server/controllers/errors.controllers.js
@@ -1,6 +1,20 @@
+exports.handlePsqlErrors = (error, request, response, next) => {
+    if(error.code === '22P02') {
+        response.status(400).send({ msg: "Bad request" });
+    } else {
+        next(error);
+    };
+};
 
+exports.handleCustomErrors = (error, request, response, next) => {
+    if(error.status) {
+        response.status(error.status).send({ msg: error.msg });
+    } else {
+        next(error);
+    };
+    
+}
 
 exports.handleServerErrors = (error, request, response, next) => {
-    console.error(error)
-    response.status(500).send("Something has broken!")
+    response.status(500).send("Something has broken!");
 }
