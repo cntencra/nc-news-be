cd9860f3d1b1ed4cf207673adeb8d38337198bf0
cntencra <cn10cra@gmail.com>
2025-03-10 15:45:32 +0000

    app.get articles

--- server/app.js
+++ server/app.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const endpointsJson = require("./endpoints.json");
 
+
 const {
     getTopics
 } = require("./controllers/topics.controllers.js");
 
 const {
+    getArticles
+} = require("./controllers/articles.controllers.js");
+
+const {
+    handlePsqlErrors,
+    handleCustomErrors,
     handleServerErrors
 } = require("./controllers/errors.controllers.js");
+
 
 const app = express();
 
@@ -17,9 +25,15 @@ app.get(`/api`, (request, response) => {
 
 app.get(`/api/topics`, getTopics);
 
+app.get(`/api/articles/:article_id`, getArticles);
+
 app.all('/*', (request, response) => {
     response.status(404).send({ msg: 'Path not found'});
 });
+
+app.use(handlePsqlErrors);
+
+app.use(handleCustomErrors);
 
 app.use(handleServerErrors);
 
