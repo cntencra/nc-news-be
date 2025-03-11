160a4a568bee1aa77b4d5a5e0fd0573e3aab4d21
cntencra <cn10cra@gmail.com>
2025-03-11 12:46:16 +0000

    deleting get and fetch coments from articles models and controllers

--- server/controllers/articles.controllers.js
+++ server/controllers/articles.controllers.js
@@ -1,7 +1,6 @@
 const {
     fetchArticles,
-    fetchArticle,
-    fetchArticleComments
+    fetchArticle
 } = require("../models/articles.models");
 
 exports.getArticles = async (request, response) => {
@@ -14,17 +13,6 @@ exports.getArticle = async (request, response, next) => {
         const { article_id } = request.params;
         const article = await fetchArticle(article_id);
         response.status(200).send({ article });
-    } catch (error) {
-        next(error);
-    };
-};
-
-exports.getArticleComments = async (request, response, next) => {
-    try {
-        const { article_id } = request.params;
-        const comments = await fetchArticleComments(article_id);
-        response.status(200).send({ comments });
-        
     } catch (error) {
         next(error);
     };
