a054faef6838cd589c84f4bdd6c88d090e93a6f5
cntencra <cn10cra@gmail.com>
2025-03-13 11:27:10 +0000

    feat: config added to Pool for connection to supabase

--- db/connection.js
+++ db/connection.js
@@ -13,16 +13,7 @@ const config = {};
 if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
-}
-
-
-const db = new Pool();
-
-if (!process.env.PGDATABASE) {
-    throw new Error("No PGDATABASE configured")
-} else { 
-    console.log(`Connected to ${process.env.PGDATABASE}`)
-}
-
+};
+const db = new Pool(config);
 
 module.exports = db;
