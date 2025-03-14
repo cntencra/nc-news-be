87e64a2c587c2a4c0533376018e986a16820b6e1
cntencra <cn10cra@gmail.com>
2025-03-14 10:37:33 +0000

    feat: controllers

--- server/controllers/users.controllers.js
+++ server/controllers/users.controllers.js
@@ -1,9 +1,23 @@
-const { fetchUsers } = require("../models/users.models")
+const { 
+    fetchUsers,
+    fetchUser
+ } = require("../models/users.models")
 
-exports.getUsers = async ( request , response ) => {
+exports.getUsers = async ( request , response, next ) => {
     try {
         const users = await fetchUsers();
         response.status(200).send({users});  
+    } catch (error) {
+        next(error);
+    };
+};
+
+exports.getUser = async ( request , response, next ) => {
+    try {
+        const { username } = request.params
+        const user = await fetchUser(username);
+        console.log(user);
+        response.status(200).send({user});  
     } catch (error) {
         next(error);
     };
