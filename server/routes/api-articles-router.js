ebf4a35123a1834bfcb4bebb8b2d889558878803
cntencra <cn10cra@gmail.com>
2025-03-14 16:07:59 +0000

    feat: routers

--- server/routes/api-articles-router.js
+++ server/routes/api-articles-router.js
@@ -3,6 +3,7 @@ const apiArticlesRouter = require("express").Router();
 const {
     getArticles,
     getArticle,
+    postArticle,
     patchArticle
 } = require("../controllers/articles.controllers.js");
 
@@ -13,9 +14,12 @@ const {
 
 apiArticlesRouter.get('', getArticles);
 
+
 apiArticlesRouter.get(`/:article_id`, getArticle);
 
 apiArticlesRouter.get(`/:article_id/comments`, getArticleComments);
+
+apiArticlesRouter.post('', postArticle);
 
 apiArticlesRouter.post(`/:article_id/comments`, postArticleComment);
 
